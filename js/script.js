079b53c6e5197c35414ed81ff2fd200836392467
Politau <paulitoruiz@gmail.com>
2023-11-20 18:52:36 +0100

    fini taille image

--- js/script.js
+++ js/script.js
@@ -3,26 +3,10 @@ let rotationTimeout;
 
 function survol(element) {
     element.style.backgroundColor = "#e0e0e0";
-    clearTimeout(rotationTimeout); // Réinitialise le timeout pour éviter la rotation continue
-    faireRotation(element, 360);
+    element.style.transform = "scale(1.1)";
 }
 
 function quitterSurvol(element) {
     element.style.backgroundColor = "#f9f9f9";
-    clearTimeout(rotationTimeout); // Annule la rotation en cours
-    element.style.transform = "scale(1)"; // Rétablit le scale à 1
-}
-
-function faireRotation(element, degresFin) {
-    let degresActuel = 0;
-
-    function animer() {
-        if (degresActuel < degresFin) {
-            degresActuel += 5; // Ajuste la vitesse de rotation ici
-            element.style.transform = `scale(1.1) rotate(${degresActuel}deg)`;
-            rotationTimeout = setTimeout(animer, 16); // Utilise setTimeout pour contrôler l'animation
-        }
-    }
-
-    animer();
+    element.style.transform = "scale(1)";
 }
